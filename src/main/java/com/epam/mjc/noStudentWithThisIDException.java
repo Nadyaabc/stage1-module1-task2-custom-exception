5cf6d497184cac9c6121c5642e0587a0518db2ca
Nadejda <nadyash295923@gmail.com>
2024-04-15 16:19:27 +0300

    plzz

--- src/main/java/com/epam/mjc/noStudentWithThisIDException.java
+++
@@ -1,9 +0,0 @@
-package com.epam.mjc;
-
-public class noStudentWithThisIDException extends IllegalArgumentException
-{
-  noStudentWithThisIDException(long ID)
-  {
-      super("Could not find student with ID " + ID);
-  }
-}
